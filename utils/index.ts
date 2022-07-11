e9c1ab0135b2d012c833851c90dd82d8cab0331f
LiuZelin <liuzelin@shoplazza.com>
2022-07-11 09:58:23 +0800

    feature: add eslint rules

--- utils/index.ts
+++ utils/index.ts
@@ -5,10 +5,10 @@ const _md5 = function(pwd:string):string {
     const md5 = crypto.createHash('md5');
     const hash = md5.update(pwd).digest('hex');
     return hash;
-}
+};
 
 export const generatePwd = function (pwd:string):string {
     pwd += salt;
     const hash = _md5(pwd);
     return hash;
-}
+};
